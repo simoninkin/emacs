07851e539c370077c76456beb14669979bdcdde3
Simon Inkin <simon.inkin@gmail.com>
2021-05-11 13:02:18 +0300

    Create ubuntu-bootstrap.sh

---
+++ ubuntu-bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# download and install fonts
+# https://github.com/nathco/Office-Code-Pro
+
+# GUI install for now
+
+
+add-apt-repository ppa:kelleyk/emacs
+
+apt update
+
+apt install emacs27
